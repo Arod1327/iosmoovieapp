1d1b57c4c3ce9e78d05e9865a68c9fe23b4bacdb
Andy Chunab <aspenkaeltas@gmail.com>
2021-02-09 11:11:49 -0600

    Initial Commit

---
+++ xcswiftapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcswiftapp
+//
+//  Created by Roger Santos on 09/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcswiftapp/xcswiftappApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcswiftappApp.swift
+//  xcswiftapp
+//
+//  Created by Roger Santos on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct xcswiftappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
