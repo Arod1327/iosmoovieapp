62aca99d683e9bf639a70f1145032c6aa0614162
Arod1327 <76719882+Arod1327@users.noreply.github.com>
2021-02-09 11:39:46 -0600

    Continue with local changes
    Creating models, bindings, uiviews, etc.

---
+++ xcswiftapp/Views/LoadingView.swift
@@ -0,0 +1,49 @@
+//
+//  LoadingView.swift
+//  SwiftUIMovieDb
+//
+//  Created by Andy Chunab on 09/02/21.
+//  Copyright © 2021 Andy Chunab. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    
+    let isLoading: Bool
+    let error: NSError?
+    let retryAction: (() -> ())?
+    
+    var body: some View {
+        Group {
+            if isLoading {
+                HStack {
+                    Spacer()
+                    ActivityIndicatorView()
+                    Spacer()
+                }
+            } else if error != nil {
+                HStack {
+                    Spacer()
+                    VStack(spacing: 4) {
+                        Text(error!.localizedDescription).font(.headline)
+                        if self.retryAction != nil {
+                            Button(action: self.retryAction!) {
+                                Text("Retry")
+                            }
+                            .foregroundColor(Color.blue)
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView(isLoading: true, error: nil, retryAction: nil)
+    }
+}
---
+++ xcswiftapp/Views/UIKitViews/SafariView.swift
@@ -0,0 +1,24 @@
+//
+//  SafariView.swift
+//  SwiftUIMovieDb
+//
+//  Created by Andy Chunab on 09/02/21.
+//  Copyright © 2021 Andy Chunab. All rights reserved.
+//
+
+import SafariServices
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        
+    }
+    
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        let safariVC = SFSafariViewController(url: self.url)
+        return safariVC
+    }
+}
